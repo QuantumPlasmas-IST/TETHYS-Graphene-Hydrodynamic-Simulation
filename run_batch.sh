24a2bd870aba647cfab70d7e69fddda4c7ede206
pcosme <pedro.cosme.s@gmail.com>
2019-11-11 15:36:25 +0000

    bash script update

--- run_batch.sh
+++ run_batch.sh
@@ -4,12 +4,12 @@ DIRNAME=$(date +"DataFiles_BATCH_%F_%H:%M")
 mkdir -p "$DIRNAME"
 
 v=10
-for s in {10,20,40,60}
+for s in 10 20 40 60
 do 
 	echo "calculating S="$s
 	echo "calculating Vf="$v
 	./Richtmyer $s $v 0 | tee -a Richt_full_output.log
-	mv -- *.dat ./$DIRNAME
+	mv -- *.dat "./$DIRNAME"
 done 
 
-mv -- *.log ./$DIRNAME
+mv -- *.log "./$DIRNAME"
