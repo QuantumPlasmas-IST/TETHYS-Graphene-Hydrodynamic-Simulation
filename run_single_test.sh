ff8fa5e672b1d4efcfb38afc3b09e70e60c9e65c
pcosme <32346364+pcosme@users.noreply.github.com>
2020-01-06 23:31:41 +0000

    Update run_single_test.sh

--- run_single_test.sh
+++ run_single_test.sh
@@ -11,7 +11,8 @@ echo "calculating S="$s
 echo "calculating Vf="$v
 echo "calculating Vc="$l
 ./Richtmyer $s $v $l 0 | tee -a Fluid.log
-./TimeSeries $(wc -l slice*) $s | tee -a TimeSeries.log
+LINECOUNT=$(wc -l slice*)
+./TimeSeries $LINECOUNT $s | tee -a TimeSeries.log
 
 mv -- Extrema* "./$DIRNAME/extrema"
 mv -- *.dat "./$DIRNAME"
