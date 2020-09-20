87f2e19903e477d82f8b1d29ac82789c7d9b286f
pcosme <pedro.cosme.s@gmail.com>
2020-09-20 18:54:29 +0100

    Versioning update 2.2.0

--- TethysLib.cpp
+++ TethysLib.cpp
@@ -111,7 +111,7 @@ cout<<"\n" ;
 	cout<<"║\033[2m      ▐█▌      ▐█▌    ▗▉      ▐█▌      ▐█▌   ▐█▌      ▐█▌    ▗       ██  \033[0m║\n";
 	cout<<"║\033[2m     ▆███▆    ▆███▆▆▆██▉     ▆███▆    ▆███▆ ▆███▆    ▆███▆   ▐█▆▆▆▆▆██▘  \033[0m║\n";
 	cout<<"║                                                                         ║\n";
-	cout<<"║ \033[1mTwo-dimensional Emitter of THz, Hydrodynamic Simulation.  Version 2.1.0\033[0m ║\n";
+	cout<<"║ \033[1mTwo-dimensional Emitter of THz, Hydrodynamic Simulation.  Version 2.2.0\033[0m ║\n";
 	cout<<"╚═════════════════════════════════════════════════════════════════════════╝\n";
 }
 
