79d8f48893455d147e29ecad5d9cc93f71676c63
pcosme <pedro.cosme.s@gmail.com>
2020-10-25 00:10:07 +0100

    testing iteration over datasets

--- src/TETHYS_2D_ElectronicAnalysis.cpp
+++ src/TETHYS_2D_ElectronicAnalysis.cpp
@@ -9,6 +9,16 @@
 using namespace std;
 
 //const H5std_string FILE_NAME( "hdf5_2D_TEST.h5" );
+herr_t
+file_info(hid_t loc_id, const char *name, void *opdata)
+{
+	/*
+	 * Display group name.
+	 */
+	cout << "Name : " << name<< endl;
+
+	return 0;
+}
 
 int main(int argc, char **argv){
 
@@ -23,8 +33,28 @@ int main(int argc, char **argv){
 	cout <<"Cyclotron frequency\t"<< parameters.CyclotronFrequency << endl;
 	cout <<"Aspect Ratio\t"<< parameters.AspectRatio << endl;
 
-	GrapheneFluid2D graph(parameters);
+	//GrapheneFluid2D graph(parameters);
 
-	cout << graph.SizeX()<<"\t"<<graph.SizeY()<<"\n";
+	//cout << graph.SizeX()<<"\t"<<graph.SizeY()<<"\n";
+
+	cout << "OPENING HDF5" <<endl;
+
+	H5std_string  FILE_NAME( "hdf5_2D_TEST.h5" );
+	H5File* hdf5_file;
+	Group* grp_dat;
+	Group* grp_den;
+	hdf5_file = new H5File(FILE_NAME, H5F_ACC_RDONLY );
+	grp_dat = new Group(hdf5_file->openGroup("/Data" ));
+	grp_den = new Group(hdf5_file->openGroup("/Data/Density" ));
+
+	cout <<"numero de coisos"<< grp_den->getNumObjs()<<endl;
+
+	for(int i=0;i<grp_den->getNumObjs();i++){
+
+	cout <<"Dataset ID:"<< i <<"\tName:"<<grp_den->getObjnameByIdx(i)<<endl;
+	}
+
+
 return 0;
 }
+
