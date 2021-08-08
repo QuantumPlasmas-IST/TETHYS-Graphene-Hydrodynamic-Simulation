255690abd86b459aad292aecf2842a67a085ec8b
pcosme <pedro.cosme.s@gmail.com>
2021-08-08 11:38:03 +0100

    implementation and testing of forced oscilations at the boundary layer to study pulsatile flow

--- src/includes/BoundaryLib.h
+++ src/includes/BoundaryLib.h
@@ -45,12 +45,10 @@ class BoundaryCondition {
 		static void YPeriodic(Fluid2D& fluid_class);       ///< periodic boundaries u(y=0)=u(y=W) for all variables
 		static void YClosedFreeSlip(Fluid2D& fluid_class); ///< zero flux across y=0 and y=W and free tangent velocity Vx
 		static void YClosedNoSlip(Fluid2D& fluid_class);   ///< zero flux across y=0 and y=W and zero tangent velocity Vx=0
+
 	static void XFreeLeft(Fluid2D &fluid_class);
-
 	static void XFreeRight(Fluid2D &fluid_class);
-
 	static void YFreeTop(Fluid2D &fluid_class);
-
 	static void YFreeBottom(Fluid2D &fluid_class);
 };
 
