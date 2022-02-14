8fee922930fd512dcb6d9de8b71412e7c5d6f7b1
pcosme <pedro.cosme.s@gmail.com>
2022-02-14 14:23:16 +0000

    Implementation of TVD schemes 3
    starting to update boundary conditions to impose on the Umain and Uaux vectors

--- src/BoundaryLib.cpp
+++ src/BoundaryLib.cpp
@@ -32,6 +32,12 @@ void BoundaryCondition::XFree(Fluid1D& fluid_class){
 	fluid_class.Den[nx - 1] =  fluid_class.Den[nx - 2];
 	fluid_class.Vel[0] = fluid_class.Vel[1];
 	fluid_class.Vel[nx - 1] = fluid_class.Vel[nx - 2];
+
+	fluid_class.Umain[0] =fluid_class.Umain[1];
+	fluid_class.Uaux[0] =fluid_class.Uaux[1];
+	fluid_class.Umain[nx-1] =fluid_class.Umain[nx-2];
+	fluid_class.Uaux[nx-1] =fluid_class.Uaux[nx-2];
+
 }
 void BoundaryCondition::XPeriodic(Fluid1D& fluid_class){
 	int nx=fluid_class.SizeX();
@@ -39,6 +45,12 @@ void BoundaryCondition::XPeriodic(Fluid1D& fluid_class){
 	fluid_class.Den[nx - 1] = fluid_class.Den[1];
 	fluid_class.Vel[0] = fluid_class.Vel[nx - 2];
 	fluid_class.Vel[nx - 1] = fluid_class.Vel[1];
+
+	fluid_class.Umain[0] =fluid_class.Umain[nx-2];
+	fluid_class.Uaux[0] =fluid_class.Uaux[nx-2];
+	fluid_class.Umain[nx-1] =fluid_class.Umain[1];
+	fluid_class.Uaux[nx-1] =fluid_class.Uaux[1];
+
 }
 void BoundaryCondition::XFree(Fluid2D& fluid_class){
 	XFree(fluid_class, 1);
