e211dc8b7ceca6a07862b9ca0036e2bd426f1729
pcosme <pedro.cosme.s@gmail.com>
2023-03-31 17:31:26 +0100

    geotrey method ot save hdf5 file draft

--- src/GeometryLib.cpp
+++ src/GeometryLib.cpp
@@ -9,6 +9,13 @@ float Geometry::top_margin(float x){
     return (0.3*x);
 }
 
+void Geometry::SaveGeometry() {
+
+	DataSet dataset_vel_snd = GrpDat->createDataSet("Sound velocity", HDF5FLOAT, *DataspaceVelSnd);
+	dataset_vel_snd.write(dominio.dom, HDF5FLOAT);
+	dataset_vel_snd.close();
+}
+
 /*bool Geometry::dom(int x, int y){
 }
 
